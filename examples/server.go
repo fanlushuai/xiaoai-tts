0c47c47e51b6ed689222377b61b6b0b3a3aa02ba
auh <21078607+fanlushuai@users.noreply.github.com>
2024-01-13 23:08:41 +0800

    fix to refresh session of xiaoai

--- examples/server.go
+++ examples/server.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	xiaoaitts "github.com/qfyang-cn/xiaoai-tts"
 	"net/http"
 	"os"
+	"time"
 )
 
-func main() {
+var x xiaoaitts.XiaoAiFunc
+
+func refreshXiaoAiSessionTicker(){
 	//系统环境变量中读取
-	//todo 可能会session失效，日后再说
 	m := &xiaoaitts.MiAccount{
 		User: os.Getenv("User"),
 		Pwd:  os.Getenv("Pwd"),
 	}
-	x := xiaoaitts.NewXiaoAi(m)
+	x = xiaoaitts.NewXiaoAi(m)
 
+	//1 个小时更新一次
+	ticker := time.NewTicker(1*time.Hour)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println(t)
+			fmt.Println("Ticker:", time.Now().Format("2006-01-02 15:04:05"))
+			x = xiaoaitts.NewXiaoAi(m)
+		}
+	}()
+
+}
+
+
+func main() {
+	refreshXiaoAiSessionTicker()
 	r := gin.Default()
 
 	r.POST("/xiaoai/speak", func(c *gin.Context) {
